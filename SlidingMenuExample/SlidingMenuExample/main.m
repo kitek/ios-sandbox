44f85cefdcf876f8dcde61a207a0fe6237a25e0b
Kitowicz Marcin <marcin.kitowicz@gmail.com>
2013-11-24 12:36:45 +0100

    Project init

---
+++ SlidingMenuExample/SlidingMenuExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SlidingMenuExample
+//
+//  Created by Kitowicz Marcin on 24.11.2013.
+//  Copyright (c) 2013 Kitek.pl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
